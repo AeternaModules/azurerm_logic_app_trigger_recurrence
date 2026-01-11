aad6f3bb60d4aac7dba578576338ade2ea2cc829
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:43:32 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "logic_app_trigger_recurrences" {
+  description = "All logic_app_trigger_recurrence resources"
+  value       = azurerm_logic_app_trigger_recurrence.logic_app_trigger_recurrences
+}
+
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "logic_app_trigger_recurrences" {
+  description = <<EOT
+Map of logic_app_trigger_recurrences, attributes below
+Required:
+    - frequency
+    - interval
+    - logic_app_id
+    - name
+Optional:
+    - start_time
+    - time_zone
+    - schedule (block):
+        - at_these_hours (optional)
+        - at_these_minutes (optional)
+        - on_these_days (optional)
+EOT
+
+  type = map(object({
+    frequency    = string
+    interval     = number
+    logic_app_id = string
+    name         = string
+    start_time   = optional(string)
+    time_zone    = optional(string)
+    schedule = optional(object({
+      at_these_hours   = optional(set(string))
+      at_these_minutes = optional(set(string))
+      on_these_days    = optional(set(string))
+    }))
+  }))
+}
+
